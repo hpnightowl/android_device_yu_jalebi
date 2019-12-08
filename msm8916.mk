ArbabAliRajper/device_samsung_j5nlte

msm8916-common: Switch to cryptfs_hw hidl service

msm8916.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
+# Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Exclude AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 

hpnightowl/android_device_yu_jalebi

Change-Id: Ie924f67f90d64609af3738f20848f08c5c26b781

msm8916.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.lineage
+
 # Exclude AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 